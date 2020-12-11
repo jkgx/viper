d3801b54ec0aaec30634c4fb2d84c27f663d1b3b
Julian <me@julian.pro>
2020-12-11 17:37:46 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/jkgx/viper
+
+go 1.15
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9
+	github.com/google/uuid v1.1.0
+	github.com/jkgx/errors v0.0.0-20201211150944-fbca1423612f
+	github.com/jkgx/jsonschema v0.0.0-20201211155603-24fde6b93ca4
+	github.com/jkgx/logrus v0.0.0-20201211153830-04212ac7fc72
+	github.com/jkgx/map v0.0.0-20201211160310-745a3d748162
+	github.com/jkgx/strings v0.0.0-20201211153405-074f8e3f9b06
+	github.com/jkgx/stringslice v0.0.0-20201211154928-71cf04bd52c0
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/ory/jsonschema/v3 v3.0.1
+	github.com/ory/viper v1.7.5
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.7.0
+	github.com/spf13/cast v1.3.1
+	github.com/spf13/cobra v1.1.1
+	github.com/stretchr/testify v1.6.1
+	github.com/tidwall/gjson v1.6.4
+	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0
+)
---
+++ validate_test.go
@@ -0,0 +1,133 @@
+package viper
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/ory/jsonschema/v3"
+	"github.com/ory/viper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidate(t *testing.T) {
+
+	path, err := filepath.Abs("./stub/config.schema.json")
+	require.NoError(t, err)
+
+	file, err := jsonschema.LoadURL("file://" + strings.Replace(path, "\\", "/", -1))
+	require.NoError(t, err)
+
+	schema, err := ioutil.ReadAll(file)
+	require.NoError(t, err)
+
+	t.Run("case=set", func(t *testing.T) {
+		viper.Reset()
+
+		viper.Set("dsn", "memory")
+		InitializeConfig(uuid.New().String(), "", nil)
+
+		require.NoError(t, Validate("schema.json", schema))
+	})
+
+	t.Run("case=missing-dsn", func(t *testing.T) {
+		viper.Reset()
+
+		InitializeConfig(uuid.New().String(), "", nil)
+
+		require.Error(t, Validate("schema.json", schema))
+	})
+
+	t.Run("case=env", func(t *testing.T) {
+		viper.Reset()
+
+		require.NoError(t, os.Setenv("DSN", "memory"))
+		InitializeConfig(uuid.New().String(), "", nil)
+
+		require.NoError(t, Validate("schema.json", schema))
+		require.NoError(t, os.Setenv("DSN", ""))
+	})
+
+	t.Run("case=file", func(t *testing.T) {
+		viper.Reset()
+
+		InitializeConfig("config", "stub", nil)
+
+		require.NoError(t, Validate("schema.json", schema))
+	})
+
+	t.Run("case=ValidateFromURL", func(t *testing.T) {
+		viper.Reset()
+
+		InitializeConfig("config", "stub", nil)
+
+		require.NoError(t, ValidateFromURL(path))
+	})
+}
+
+func TestLoggerWithValidationErrorFields(t *testing.T) {
+	path, err := filepath.Abs("./stub/config.schema.json")
+	require.NoError(t, err)
+
+	testPath := "file://" + strings.Replace(path, "\\", "/", -1)
+	t.Log(testPath)
+
+	t.Run("case=required", func(t *testing.T) {
+		viper.Reset()
+
+		err := ValidateFromURL(testPath)
+		require.Error(t, err)
+
+		var b bytes.Buffer
+		PrintHumanReadableValidationErrors(&b, err)
+		assert.Contains(t, b.String(), "one or more required properties are missing")
+	})
+
+	t.Run("case=type", func(t *testing.T) {
+		viper.Reset()
+
+		viper.Set("dsn", 1234)
+		err := ValidateFromURL(testPath)
+		require.Error(t, err)
+
+		var b bytes.Buffer
+		PrintHumanReadableValidationErrors(&b, err)
+		assert.Contains(t, b.String(), "expected string, but got number")
+	})
+
+	t.Run("case=multiple errors", func(t *testing.T) {
+		viper.Reset()
+
+		viper.Set("dsn", 1234)
+		viper.Set("foo", 1234)
+		err := ValidateFromURL(testPath)
+		require.Error(t, err)
+
+		expected := []struct {
+			key string
+			err string
+		}{
+			{
+				key: "dsn",
+				err: "expected string, but got number",
+			},
+			{
+				key: "foo",
+				err: "value must be \"bar\"",
+			},
+		}
+
+		var b bytes.Buffer
+		PrintHumanReadableValidationErrors(&b, err)
+		jsonString := b.String()
+		for _, e := range expected {
+			assert.Contains(t, b.String(), e.err)
+			assert.Contains(t, b.String(), e.key, "%s", jsonString)
+		}
+	})
+}
